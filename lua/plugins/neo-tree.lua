e8808a0c63a60dbcb86f750286ee5495ce2288d5
ujjwal bhandari <80826873+ujjwalbhandarii@users.noreply.github.com>
2024-10-20 14:32:59 +0545

    Update neo-tree.lua

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,26 +2,24 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
     window = {
-      position = "right", -- Move the file tree to the right
-      width = 60, -- Adjust the width as needed
-      -- You can also set the height if you want to restrict it vertically
+      position = "right", 
+      width = 60, 
     },
     filesystem = {
       filtered_items = {
-        visible = true, -- Show hidden files
-        hide_dotfiles = false, -- Do not hide dotfiles (files starting with a .)
-        hide_gitignored = false, -- Show git ignored files as well (optional)
+        visible = true, 
+        hide_dotfiles = false,
+        hide_gitignored = false,
       },
       follow_current_file = {
         enabled = true,
       }, -- Focus on the current file in the tree
       group_empty_dirs = true, -- Group empty directories together
-      -- You can also add other filtering options if needed
     },
     default_component_configs = {
       indent = {
-        indent_size = 2, -- Set indent size
-        padding = 1, -- Extra padding for indent
+        indent_size = 2, 
+        padding = 1, 
       },
       icon = {
         folder_empty = "📁", -- Set icon for empty folders (customize if needed)
